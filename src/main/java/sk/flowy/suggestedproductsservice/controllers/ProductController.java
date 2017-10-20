138f8b0826828902adb980daff722b5e6e602fca
Jaroslav Regec <jaroslav.regec@codexa.sk>
2017-10-20 12:20:55 +0200

    FLOW-113 added mock mvc tests

--- src/main/java/sk/flowy/suggestedproductsservice/controllers/ProductController.java
+++ src/main/java/sk/flowy/suggestedproductsservice/controllers/ProductController.java
@@ -43,5 +43,4 @@ public class ProductController {
         }
         return new ResponseEntity<>(product, OK);
     }
-
 }
--- src/test/java/sk/flowy/suggestedproductsservice/controllers/ProductControllerTest.java
+++ src/test/java/sk/flowy/suggestedproductsservice/controllers/ProductControllerTest.java
@@ -5,37 +5,50 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import sk.flowy.suggestedproductsservice.TestConfiguration;
-import sk.flowy.suggestedproductsservice.exceptions.ProductNotFoundException;
 import sk.flowy.suggestedproductsservice.model.Ean;
 import sk.flowy.suggestedproductsservice.model.Product;
-import sk.flowy.suggestedproductsservice.repository.EanRepository;
+import sk.flowy.suggestedproductsservice.model.ReceiptProduct;
+import sk.flowy.suggestedproductsservice.security.CallResponse;
+import sk.flowy.suggestedproductsservice.security.TokenRepository;
+import sk.flowy.suggestedproductsservice.services.EanService;
 
 import static java.util.Collections.singletonList;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sk.flowy.suggestedproductsservice.utils.ProductUtils.convertProduct;
 
 @RunWith(SpringRunner.class)
-@Import(TestConfiguration.class)
 @WebMvcTest(ProductController.class)
 public class ProductControllerTest {
+
+    private static final String VALID_TOKEN = "Bearer " +
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjYyLCJpc3MiOiJodHRwOlwvXC9sYWJhcy5wcm9taXNlby5jb21cL2FwcFwvcHVibGljXC9hcGlcL2F1dGhlbnRpY2F0ZSIsImlhdCI6MTUwODIzMjIyMSwiZXhwIjoxNTA4NDI0MjIxLCJuYmYiOjE1MDgyMzIyMjEsImp0aSI6IjdlMmQ1NTg2Yzg4YjAzMzE0NDAzZDRmM2U3ODIyMjAzIn0.ujt2PXYrlmHVcL1GZo5ByZHcHdBaPr-dkCgtvBx8uG8";
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String RIGHT_EAN = "123456";
+    private static final String INVALID_EAN = "invalid";
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private EanRepository eanRepository;
+    @MockBean
+    private EanService eanService;
+
+    @MockBean
+    private TokenRepository tokenRepository;
+
+    private ReceiptProduct receiptProduct;
 
     @Before
     public void setup() {
         Ean ean = new Ean();
-        ean.setValue("1234560");
+        ean.setValue(RIGHT_EAN);
 
         Product product = new Product();
         product.setEans(singletonList(new Ean()));
@@ -43,18 +56,24 @@ public class ProductControllerTest {
         product.setName("Horalka");
         ean.setProduct(product);
 
-        when(eanRepository.findByValue("1234560")).thenReturn(ean);
-        when(eanRepository.findByValue("invalid")).thenReturn(null);
+        receiptProduct = convertProduct(product);
+
+        when(tokenRepository.checkTokenValidity(anyString())).thenReturn(new CallResponse("success", null));
     }
 
     @Test
     public void if_ean_is_in_database_then_right_product_is_returned() throws Exception {
-        mockMvc.perform(get("/api/product/1234560").accept(APPLICATION_JSON)).andExpect(status().isOk())
-                .andExpect(content().string("Honda Civic"));
+        when(eanService.getProductByEan(RIGHT_EAN)).thenReturn(receiptProduct);
+        mockMvc.perform(get("/api/product/" + RIGHT_EAN).header(AUTHORIZATION, VALID_TOKEN).accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":10,\"name\":\"Horalka\",\"eans\":[{\"id\":null,\"type\":null," +
+                        "\"value\":null,\"createdAt\":null,\"updatedAt\":null,\"deletedAt\":null}]}"));
     }
 
-    @Test(expected = ProductNotFoundException.class)
-    public void if_ean_is_not_found_exception_is_thrown() {
-
+    @Test
+    public void if_ean_is_not_found_exception_is_thrown() throws Exception {
+        when(eanService.getProductByEan(INVALID_EAN)).thenReturn(null);
+        mockMvc.perform(get("/api/product/" + INVALID_EAN).header(AUTHORIZATION, VALID_TOKEN).accept(APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
